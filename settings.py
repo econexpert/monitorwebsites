23f31f83b7c3595a88d3f697da21b3eedb7a5d3d
econexpert <econexpert@users.noreply.github.com>
2023-07-28 13:45:06 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,6 @@
+#  MongoDb connection string, to be kept secret, replace new_user_name_for_python and new_user_password_for_python with database user login and password, white list your IP address to MongoDb
+#  more on https://www.mongodb.com/docs/atlas/tutorial/connect-to-your-cluster/
+#   usually something like this:  "mongodb+srv://new_user_name_for_python:new_user_password_for_python@better_copy_string_from_atlas.mongodb.net/"      
+URI = "mongodb+srv://new_user_name_for_python:new_user_password_for_python@better_copy_string_from_atlas.mongodb.net/"
+DATABASE = "yourdatabasename"  # change datebase name here
+COLLECTION = "yourcollectionname" # change collection name here
