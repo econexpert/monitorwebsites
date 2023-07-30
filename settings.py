f8c222b27d452dc1e1a84314642e79bc9731fc40
econexpert <econexpert@users.noreply.github.com>
2023-07-31 00:23:08 +0300

    Update settings.py
    added KEYWORDS field

--- settings.py
+++ settings.py
@@ -4,3 +4,6 @@
 URI = "mongodb+srv://new_user_name_for_python:new_user_password_for_python@better_copy_string_from_atlas.mongodb.net/"
 DATABASE = "yourdatabasename"  # change datebase name here
 COLLECTION = "yourcollectionname" # change collection name here
+# comma seperated keywords to search in added lines
+KEYWORDS = "keyword1,Europe,technology"  # for example looking for keywords Europe or technology
+
